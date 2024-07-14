9efe1861cbd6e59ec5d2b4a3f9da670fd250e795
Ian McNicoll <ian@freshehr.com>
2024-07-14 17:39:56 +0100

    Added fsh-Logical support

--- src/formatters/FshLogicalModelFormatter.ts
+++ src/formatters/FshLogicalModelFormatter.ts
@@ -2,32 +2,12 @@ import fs from "fs";
 
 import { DocBuilder } from "../DocBuilder";
 import {  TemplateNode, TemplateInput } from "../TemplateNodes";
-import { formatOccurrences, isAnyChoice, mapRmType2FHIR, mapRmTypeText } from '../TemplateTypes';
-import { formatRawOccurrencesText } from "./DocFormatter";
+import { formatOccurrences, mapRmType2FHIR} from '../TemplateTypes';
 
-
-const headerIndent: string = '  -';
-const eventIndent:  string = '    -';
-const nodeIndent:   string = '      -';
-const dvIndent:     string = '        -';
-
-function extractTextInBrackets(input: string): string[] {
-  // The regular expression matches text within square brackets.
-  const regex = /\[(.*?)]/g;
-  let match;
-  const matches: string[] = [];
-
-  // tslint:disable-next-line:no-conditional-assignment
-  while ((match = regex.exec(input)) !== null) {
-    matches.push(match[1]);
-  }
-
-  return matches;
-}
 export const fshl = {
 
   formatTemplateHeader: (dBuilder: DocBuilder) => {
-    const { wt,sb, config } = dBuilder;
+    const { wt, sb, config } = dBuilder;
 
     const techName = wt.templateId.toLowerCase().replace(/ /g, "_");
     sb.append(`Logical: ${techName}`)
@@ -35,26 +15,26 @@ export const fshl = {
   },
 
   formatCompositionHeader: (dBuilder: DocBuilder, f: TemplateNode) => {
-    const { wt,sb, config } = dBuilder;
+    const { wt, sb, config } = dBuilder;
     sb.append(`Description:  "${f.localizedDescriptions.en}"`)
     sb.append(`* ^name = ${f.id}`)
     sb.append(`* ^status = #active`)
 
   },
   formatCompositionContextHeader: (dBuilder: DocBuilder, f: TemplateNode) => {
-    const { sb} = dBuilder;
+    const { sb } = dBuilder;
     const localName = f.localizedName ? f.localizedName : f.name
     const nodeName = f.id
     const rmTypeText = mapRmType2FHIR(f.rmType);
-    const occurrencesText = formatOccurrences(f,  true)
+    const occurrencesText = formatOccurrences(f, true)
     const localizedDescription = dBuilder.getDescription(f)
-    const spaces : string = " ".repeat(f.depth);
+    const spaces: string = " ".repeat(f.depth);
     sb.append(`${spaces} * ${nodeName} ${occurrencesText} ${rmTypeText} "${localName}" "${f.nodeId}: ${localizedDescription}"`)
 
   },
 
-  saveFile: async (dBuilder: DocBuilder, outFile: any): Promise <void>  => {
-    fs.writeFileSync(outFile, dBuilder.toString(), {encoding: "utf8"});
+  saveFile: async (dBuilder: DocBuilder, outFile: any): Promise<void> => {
+    fs.writeFileSync(outFile, dBuilder.toString(), { encoding: "utf8" });
     console.log(`\n Exported : ${outFile}`)
   },
 
@@ -66,9 +46,9 @@ export const fshl = {
     const localName = f.localizedName ? f.localizedName : f.name
     const nodeName = f.id
     const rmTypeText = mapRmType2FHIR(f.rmType);
-    const occurrencesText = formatOccurrences(f,  true)
+    const occurrencesText = formatOccurrences(f, true)
     const localizedDescription = dBuilder.getDescription(f)
-    const spaces : string = " ".repeat(f.depth*2);
+    const spaces: string = " ".repeat(f.depth * 2);
 
     sb.append(`${spaces}* ${nodeName} ${occurrencesText} ${rmTypeText} "${localName}" "${localizedDescription}"`)
   },
@@ -78,9 +58,9 @@ export const fshl = {
     const localName = f.localizedName ? f.localizedName : f.name
     const nodeName = f.id
     const rmTypeText = mapRmType2FHIR(f.rmType);
-    const occurrencesText = formatOccurrences(f,  true)
+    const occurrencesText = formatOccurrences(f, true)
     const localizedDescription = dBuilder.getDescription(f)
-    const spaces : string = " ".repeat(f.depth*2);
+    const spaces: string = " ".repeat(f.depth * 2);
 
     sb.append(`${spaces}* ${nodeName} ${occurrencesText} ${rmTypeText} "${localName}" "${f.nodeId}: ${localizedDescription}"`)
   },
@@ -90,112 +70,47 @@ export const fshl = {
     const localName = f.localizedName ? f.localizedName : f.name
     const nodeName = f.id
     const rmTypeText = mapRmType2FHIR(f.rmType);
-    const occurrencesText = formatOccurrences(f,  true)
+    const occurrencesText = formatOccurrences(f, true)
     const localizedDescription = dBuilder.getDescription(f)
-    const spaces : string = " ".repeat(f.depth*2);
+    const spaces: string = " ".repeat(f.depth * 2);
 
     sb.append(`${spaces}* ${nodeName} ${occurrencesText} ${rmTypeText} "${localName}" "${f.nodeId}: ${localizedDescription}"`)
   },
 
   formatObservationEvent: (dBuilder: DocBuilder, f: TemplateNode) => {
-    const { sb} = dBuilder;
+    const { sb } = dBuilder;
     const localName = f.localizedName ? f.localizedName : f.name
     const nodeName = f.id
     const rmTypeText = mapRmType2FHIR(f.rmType);
-    const occurrencesText = formatOccurrences(f,  true)
+    const occurrencesText = formatOccurrences(f, true)
     const localizedDescription = dBuilder.getDescription(f)
-    const spaces : string = " ".repeat(f.depth*2);
+    const spaces: string = " ".repeat(f.depth * 2);
 
     sb.append(`${spaces}* ${nodeName} ${occurrencesText} ${rmTypeText} "${localName}" "${f.rmType}: ${localizedDescription}"`)
   },
 
   formatChoiceHeader: (dBuilder: DocBuilder, f: TemplateNode) => {
-      const { sb, config } = dBuilder;
-      const localName = f.localizedName ? f.localizedName : f.name
-      const nodeName = f.id
+    const { sb, config } = dBuilder;
+    const localName = f.localizedName ? f.localizedName : f.name
+    const nodeName = f.id
 
-      const occurrencesText = formatOccurrences(f,  true)
-      const localizedDescription = dBuilder.getDescription(f)
-      const spaces : string = " ".repeat(f.depth*2);
+    const occurrencesText = formatOccurrences(f, true)
+    const localizedDescription = dBuilder.getDescription(f)
+    const spaces: string = " ".repeat(f.depth * 2);
 
     let rmTypeText = '';
-    let newText : string = ''
+    let newText: string = ''
     f.children.forEach((child) => {
       child.parentNode = f
       newText = mapRmType2FHIR(child.rmType)
       if ((rmTypeText.length) === 0)
         rmTypeText = newText
-      else
-      {
-        if(!rmTypeText.includes(newText))
-         rmTypeText = rmTypeText.concat(' or ' + newText)
+      else {
+        if (!rmTypeText.includes(newText))
+          rmTypeText = rmTypeText.concat(' or ' + newText)
       }
     });
 
     sb.append(`${spaces}* ${nodeName} ${occurrencesText} ${rmTypeText} "${localName}" "${f.nodeId}: ${localizedDescription}"`)
-    },
-
-  dvTypes: {
-    formatDvCodedText: (dBuilder: DocBuilder, f: TemplateNode) => {
-      const { sb, config} = dBuilder;
-
-      f?.inputs.forEach((item :TemplateInput) => {
-        if (item.list ) {
-          item.list.forEach((list) => {
-          if (!config.hideXmindValues)
-            sb.append(`${dvIndent} ${list.label} [B]`);
-          })
-        } else
-          // Pick up an external valueset description annotation
-        if (item.suffix === 'code' && f?.annotations?.vset_description) {
-          // Convert /n characters to linebreaks
-          const extRef = extractTextInBrackets(f.annotations?.vset_description)
-          sb.append(`${dvIndent} ${extRef[0]} [B]`)
-        }
-
-        if (item.listOpen  && !config.hideXmindValues)
-          sb.append( `${dvIndent} Other text/ coded text allowed [B]`);
-      });
-    },
-
-    formatDvText: (dBuilder: DocBuilder, f: TemplateNode) => {
-      const { sb , config} = dBuilder;
-
-      //  * Indikation 0..* CodeableConcept "Indikation" "Indikation;
-      if (f.inputs.length > 0) {
-        f.inputs.forEach((input) => {
-          if (input.list ) {
-            input.list.forEach((listItem) => {
-              if (!config.hideXmindValues)
-                sb.append(`${dvIndent} ${listItem.value} [B]`)
-            })
-          }
-          else
-          if (input.suffix !== 'other' && f?.annotations?.vset_description) {
-            // Pick up an external valueset description annotation
-            const extRef = extractTextInBrackets(f.annotations?.vset_description)
-            sb.append(`${dvIndent} ${extRef[0]} [B]`)
-          }
-
-          if (input.listOpen  && !config.hideXmindValues)
-            sb.append(`${dvIndent} Other text/coded text allowed [B]`);
-
-        });
-//      appendDescription(f);
-      }
-    },
-
-    formatDvOrdinal: (dBuilder: DocBuilder, f: TemplateNode) => {
-      const { sb , config} = dBuilder;
-
-      f.inputs.forEach((input) => {
-        if (input.list)
-          input.list.forEach((listItem) => {
-          if (!config.hideXmindValues)
-            sb.append(`${dvIndent} (${listItem.ordinal}) ${listItem.label} [B]`)
-          })
-      })
-    },
-
-  }
+  },
 }
